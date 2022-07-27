b64729a5927ed7ada042decfcf3e5ede300cc3f4
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-27 11:41:35 +0300

    be colbac

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 'use strict';
 console.log('labas main.js');
+
+function displayH2(str) {
+    const h2El = document.getElementById('h2');
+    h2El.textContent = str;
+}
+
+function displayAsParagraf(value){
+    const paraEl = document.getElementById('para');
+    paraEl.textContent = `the value is ${value} !!!`;
+}
+
+displayH2('Labas pasauli');
+
+function myPower(num){    
+const rezult = num ** 3;
+return rezult;
+}
+const rezult = myPower(3);   // nuoseklesnis budas
+
+displayH2(rezult);
+displayAsParagraf(rezult);
+
+// displayH2(myPower(3));    // matrioska
+console.log('funkcija my power', displayH2);
+// kai iskvieciam funkcija ir nepaduodam argumento, gauname funkcijos aprasyma 'displayH2(myPower);'
